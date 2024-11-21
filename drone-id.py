2a46d9702e9ac753e613fe817d18122ea66d807d
nickusafa <nickfsu1102@gmail.com>
2024-11-21 11:50:40 -0700

    Fixed bugs in drone-id.py

--- drone-id.py
+++ drone-id.py
@@ -5,13 +5,18 @@ bit1 = Button(pin=27,pull_up=False)
 bit2 = Button(pin=17,pull_up=False)
 sw = Button(pin=5,pull_up=False)
 
-id2 = bit2.is_pressed ? 4 : 0
-id1 = bit1.is_pressed ? 2 : 0
-id0 = bit0.is_pressed ? 1 : 0
-id = id2+id1+id0
+id = 0
+id = id + 4 if bit2.is_pressed else id
+id = id + 2 if bit1.is_pressed else id
+id = id + 1 if bit0.is_pressed else id
 
 print("Drone ID: ",id)
 
+was_pressed = 0
 while True:
   if sw.is_pressed:
-    print("Pressed")
+    if (was_pressed == 0 and sw.is_pressed == 1):
+      print("Pressed")
+    was_pressed = 1
+  else:
+    was_pressed = 0
