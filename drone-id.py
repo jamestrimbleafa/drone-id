3481fdb5a33bea89b14c34a955c5246fe1f88f74
nickusafa <nickfsu1102@gmail.com>
2024-11-21 16:20:02 -0700

    Added command line argument to python version

--- drone-id.py
+++ drone-id.py
@@ -1,22 +1,31 @@
+import argparse
 from gpiozero import Button
 
-bit0 = Button(pin=22,pull_up=False)
-bit1 = Button(pin=27,pull_up=False)
-bit2 = Button(pin=17,pull_up=False)
-sw = Button(pin=5,pull_up=False)
+# Initialize buttons
+bit0 = Button(pin=22, pull_up=False)
+bit1 = Button(pin=27, pull_up=False)
+bit2 = Button(pin=17, pull_up=False)
+sw0 = Button(pin=5, pull_up=False)
 
+# Calculate drone ID
 id = 0
 id = id + 4 if bit2.is_pressed else id
 id = id + 2 if bit1.is_pressed else id
 id = id + 1 if bit0.is_pressed else id
 
-print("Drone ID: ",id)
+# Read switch state
+sw = 1 if sw0.is_pressed else 0
 
-was_pressed = 0
-while True:
-  if sw.is_pressed:
-    if (was_pressed == 0 and sw.is_pressed == 1):
-      print("Pressed")
-    was_pressed = 1
-  else:
-    was_pressed = 0
+# Set up argument parser
+parser = argparse.ArgumentParser(description='Drone ID and Switch Status')
+parser.add_argument('--value', action='store_true', help='Only print the Drone ID')
+
+# Parse command-line arguments
+args = parser.parse_args()
+
+# Print output based on the presence of the --value flag
+if args.value:
+    print(id)  # Only print the ID when --value is specified
+else:
+    print("Drone ID: ", id)
+    print("Switch: ", sw)
