abb1df12459bb0af7f1b8d9559ef041280a80177
nickusafa <nickfsu1102@gmail.com>
2024-11-21 16:10:58 -0700

    Updated c code touse libgpiod

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 drone-id:
 	@echo "Compiling drone-id"
-	gcc -o drone-id ./drone-id.c 
+	gcc drone-id.c -lgpiod -o drone-id
 
 clean:
 	@echo "Removing drone-id"
 	rm -f ./drone-id
 
 configure:
+	@echo "Installing gpiod and C libraries"
 	sudo apt install gpiod libgpiod-dev
+	@echo "Installing python gpio zero package"
+	pip install gpiozero
--- drone-id.c
+++ drone-id.c
@@ -1,72 +1,68 @@
-#include <stdio.h>
+#include <gpiod.h>
+#include <error.h>
 #include <stdlib.h>
-#include <linux/gpio.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/ioctl.h>
-#include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
+struct gpiod_chip *chip;
+struct gpiod_line_request_config config;
+struct gpiod_line_bulk lines;
 
-#define BIT2 17 // Physical pin 11 / GPIO17 (GPIO_GEN0)
-#define BIT1 27 // Physical pin 13 / GPIO27 (GPIO_GEN2)
-#define BIT0 22 // Physical pin 15 / GPIO22 (GPIO_GEN3)
+int
+main(int argc, char *argv[])
+{
+  unsigned int offsets[4] = {22, 27, 17, 5};
 
-#define DEV_NAME "/dev/gpiochip0"
+  int values[4] = {-1, -1, -1, -1};
+  int err;
 
-int main(int argc, char* argv[]) {
-	int id = -1;
+  chip = gpiod_chip_open("/dev/gpiochip0");
+  if(!chip)
+  {
+    perror("gpiod_chip_open");
+    goto cleanup;
+  }
 
-	// Initialize GPIO
-	
-	int fd, ret;
-	fd = open(DEV_NAME, O_RDONLY);
-	if (fd < 0) {
-		printf("Unable to open %s: %s\n", DEV_NAME, strerror(errno));
-		return 0;
-	}
+  err = gpiod_chip_get_lines(chip, offsets, 4, &lines);
+  if(err)
+  {
+    perror("gpiod_chip_get_lines");
+    goto cleanup;
+  }
 
+  memset(&config, 0, sizeof(config));
+  config.consumer = "drone-id";
+  config.request_type = GPIOD_LINE_REQUEST_DIRECTION_INPUT;
+  config.flags = 0;
 
-	struct gpiohandle_request rq;
-	struct gpiohandle_data data;
+  err = gpiod_line_request_bulk(&lines, &config, values);
+  if(err)
+  {
+    perror("gpiod_line_request_bulk");
+    goto cleanup;
+  }
 
-	rq.lineoffsets[0] = BIT0;
-	rq.lineoffsets[1] = BIT1;
-	rq.lineoffsets[2] = BIT2;
-	rq.flags = GPIOHANDLE_REQUEST_INPUT;
-	rq.lines = 3;
-	ret = ioctl(fd, GPIO_GET_LINEHANDLE_IOCTL, &rq);
+  err = gpiod_line_get_value_bulk(&lines, values);
+  if(err)
+  {
+    perror("gpiod_line_get_value_bulk");
+    goto cleanup;
+  }
 
-	close(fd);
+  int id = values[2]*4 + values[1]*2 + values[0];
+  int sw = values[3];
 
-	// Setup each GPIO pin as input with a pulldown resistor.  This way the pin will read zero/false when grounded or not connected, and one/true when connected to 3.3V
-	
-	if (ret == -1) {
-		printf("Unable to get line handle from ioctl: %s",strerror(errno));
-		return 0;
-	}
-	ret = ioctl(rq.fd, GPIOHANDLE_GET_LINE_VALUES_IOCTL, &data);
-	if (ret == -1) {
-		printf("Unable to get line handle from ioctl: %s", strerror(errno));
-	} else {	
-		//printf("17: %d, 27: %d, 22: %d\n",data.values[0], data.values[1], data.values[2]);
-		
-		id = 0;
-		id += (4*data.values[2]);
-		id += (2*data.values[1]);
-		id += (1*data.values[0]);
-	}
-	close(rq.fd);
-
-	// Output the done id if successful or -1 if there was an error
-	if (argc > 1 && strcmp(argv[1],"--value")==0) {
+  if (argc > 1 && strcmp(argv[1],"--value")==0) {
 		printf("%d",id);
 	} else {
 		printf("Drone ID: %d\n",id);
-	}
+		printf("Switch: %d\n", sw);
+  }  
 
-	// Terminate
-	return 0;
+cleanup:
+  gpiod_line_release_bulk(&lines);
+  gpiod_chip_close(chip);
+
+  return EXIT_SUCCESS;
 }
-
